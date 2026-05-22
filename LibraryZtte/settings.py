25f6690e639aff2e419d4fb02be259be806c23b5
IFedenko <minusakkgp@gmail.com>
2026-05-22 20:31:48 +0500

    Обновление settings.py

--- LibraryZtte/settings.py
+++ LibraryZtte/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ctfwq-5y8p8%ldqy%r_rwex9*)69dq%etj@4)fgy)m!-hnpve5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -124,7 +124,7 @@ DEFAULT_CHARSET = 'utf-8'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
